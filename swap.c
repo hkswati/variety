3f22413a1b9f2ee7071974b8c5fcdb32f753dad5
hkswati <hkswati@users.noreply.github.com>
2019-05-01 23:53:11 +0300

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char* argv[]){
+  
+  int a = atoi(argv[1]);
+  int b  = atoi(argv[2]);
+  printf("Initial value of a: %d\n", a);
+  printf("Initial value of b: %d\n", b);
+  a = a+b;
+  b = a-b;
+  a = a-b;
+  printf("Swapped value of a: %d\n", a);
+  printf("Swapped value of b: %d\n", b);
+  
+}
