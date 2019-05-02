9a4089c554ee1d185821f5238b5bc7a8bd1ccfc4
hkswati <hkswati@users.noreply.github.com>
2019-05-02 23:58:53 +0300

    Update swap.c

--- swap.c
+++ swap.c
@@ -14,3 +14,23 @@ int main(int argc, char* argv[]){
   printf("Swapped value of b: %d\n", b);
   
 }
+
+
+
+/*SQL queries:
+SELECT COUNT(*)
+FROM (SELECT userId, COUNT(userId) AS login_count
+	  FROM 	i_user_login_logs
+	  GROUP BY userId
+	  HAVING login_count > 3);
+
+
+
+SELECT username, emailaddress
+FROM i_users
+WHERE userId in (SELECT userId, MAX(login_count)
+				 FROM (SELECT userId, COUNT(userId) AS login_count
+	  					FROM 	i_user_login_logs
+	  					GROUP BY userId));
+*/              
+              
